30fe49671db17efe36b857750ccd361fbeca3a99
yulizarw <yulizarwidiatama@gmail.com>
2026-08-19 02:17:26 +0700

    sudah letakin deployan be

--- smartagrifereact/src/config/axios.jsx
+++ smartagrifereact/src/config/axios.jsx
@@ -1,8 +1,8 @@
 import axios from 'axios'
 
-const instance  = axios.create({
-  baseURL:'http://localhost:3001/'
-//   baseURL:'https://avesboxbe.herokuapp.com/'
-})
+const instance = axios.create({
+  // baseURL:'http://localhost:3001/'
+  baseURL: "http://16.170.252.125",
+});
 
 export default instance
